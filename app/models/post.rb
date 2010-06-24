7645b877dda53f8d2ed088304bcfffee0e704194
Frank <frank_fan@huieee.com>
2010-06-24 16:36:22 +0800

    add uniqueness validation to post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
 	has_many :comments, :dependent => :destroy
 	
 	validates_presence_of :title , :content ,:published_at
+	validates_uniqueness_of :title
 	named_scope :published, lambda { {:conditions => ['published_at <= ?', Time.now.utc]} }
     named_scope :unpublished, lambda { {:conditions => ['published_at > ?', Time.now.utc]} }
     named_scope :recent, :order => 'id DESC'
