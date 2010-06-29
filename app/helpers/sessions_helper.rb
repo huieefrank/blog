3022b9784d80dd30975d6127395aa042250af0f1
Frank <frank_fan@huieee.com>
2010-06-29 10:29:35 +0800

    complete user edit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,10 @@ module SessionsHelper
 		self.current_user = nil
 	end
 	
+	def current_user?(user)
+		user == current_user
+	end
+	
 	def deny_access
 		flash[:notice] = "Please sign in to access this page."
 	redirect_to signin_path
